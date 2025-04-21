0e02b58a468b181e8677b14fd6fcce0061b91dba
Abhijit Singh <abhijitchau8948@gmail.com>
2025-04-21 16:27:17 +0530

    added the 3rd pattern

---
+++ Abhijit Singh/Day1/DiamondPattern.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int userInput = scanner.nextInt();
+        System.out.print("Enter an character: ");
+        char userInputChar = scanner.next().charAt(0); 
+
+        // Top half
+        for (int i = 1; i <= userInput; i++) {
+            // Left stars
+            for (int j = 1; j <= userInput - i; j++) {
+                System.out.print(" ");
+            }
+            // Middle spaces
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                // Keep borders as stars
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(userInputChar);
+                }
+            }
+            // Right stars 
+            for (int j = 1; j <= userInput - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        // Bottom half
+        for (int i = userInput - 1; i >= 1; i--) {
+            // Left stars
+            for (int j = 1; j <= userInput - i; j++) {
+                System.out.print(" ");
+            }
+            // Middle spaces
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                if (j == 1 || j == (2 * i - 1)) {
+                    System.out.print(" ");
+                } else {
+                    System.out.print(userInputChar);
+                }
+            }
+            // Right stars
+            for (int j = 1; j <= userInput - i; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
