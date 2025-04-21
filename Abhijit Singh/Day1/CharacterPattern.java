f49f5f1bb76964826ffe36c2c69f03404917d322
Abhijit Singh <abhijitchau8948@gmail.com>
2025-04-21 16:04:10 +0530

    completed the assignment

--- Abhijit Singh/Day1/CharacterPattern.java
+++ Abhijit Singh/Day1/CharacterPattern.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class CharacterPattern {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of rows");
         int height = scanner.nextInt();
         int width = 2 * height - 1;
         int mid = height / 2;
