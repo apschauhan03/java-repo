064f0e79545afe7dba3f398284f5525091d27bea
Abhijit Singh <abhijitchau8948@gmail.com>
2025-04-24 13:29:03 +0530

    Day4

--- AbhijitSingh/Day4/MapImplementation.java
+++ AbhijitSingh/Day4/MapImplementation.java
@@ -1,21 +1,34 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MapImplementation {
     public static void main(String[] args) { 
-        Map<Integer, String> mp = new HashMap<>();
-
-        mp.put(1, "a");
-        mp.put(2, "b");
-        mp.put(3, "c");
-        mp.put(4, "c");
-        mp.remove(3);
+        Map<String, ArrayList<Integer>> mp = new HashMap<>();
+        ArrayList<Integer> arr = new ArrayList<>();
+        mp.put("a", arr);
+        mp.put("b", arr);
+        mp.put("c", arr);
+        mp.put("d", arr);
+        mp.remove("a");
         
         
-        if (mp.containsValue("d"))
+        if (mp.containsValue(arr))
             System.out.println(mp.keySet());
         
         System.out.println(mp.entrySet());
+
+
+        ArrayList<Integer> temp = mp.get("b");
+
+        temp.add(7);
+
+        if(temp.contains(1))
+            System.out.println("Contains 1");
+
+        for (Integer integer : temp) {
+            System.out.println(integer);
+        }
 
     }
 }
